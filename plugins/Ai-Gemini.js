44b1b843ace30c63a7b1613c0494cf0ae3c202f8
Alba070503 <97574964+Alba070503@users.noreply.github.com>
2025-02-21 21:51:37 -0400

    Update Ai-Gemini.js

--- plugins/Ai-Gemini.js
+++ plugins/Ai-Gemini.js
@@ -1,30 +1,56 @@
-import fetch from 'node-fetch';
+/* 
 
-const handler = async (m, { conn, text }) => {
-  if (!text) {
-    return conn.reply(m.chat, '❀ Ingrese una petición para que Gemini lo responda.', m);
-  }
+❀ By JTxs
 
-  try {
-    await m.react(rwait);
-    const response = await fetch(`https://apis-starlights-team.koyeb.app/starlight/gemini?text=${encodeURIComponent(text)}`);
-    const json = await response.json();
+[ Canal Principal ] :
+https://whatsapp.com/channel/0029VaeQcFXEFeXtNMHk0D0n
 
-    if (!json || !json.result) {
-      throw new Error('Respuesta inválida de la API');
-    }
+[ Canal Rikka Takanashi Bot ] :
+https://whatsapp.com/channel/0029VaksDf4I1rcsIO6Rip2X
 
-    await conn.reply(m.chat, json.result, m);
-    await m.react(done);
-  } catch (e) {
-    console.error('Error en el comando Gemini:', e);
-    await m.react(error);
-    await conn.reply(m.chat, '✘ Gemini no puede responder a esa pregunta.', m);
-  }
-};
+[ Canal StarlightsTeam] :
+https://whatsapp.com/channel/0029VaBfsIwGk1FyaqFcK91S
 
-handler.command = ['gemini'];
-handler.help = ['gemini'];
-handler.tags = ['ai'];
+[ HasumiBot FreeCodes ] :
+https://whatsapp.com/channel/0029Vanjyqb2f3ERifCpGT0W
+*/
 
-export default handler;
+//  $ npm install groq-sdk
+// Package : "groq-sdk": "latest"
+// *[ ❀ LLAMA AI - PROMPT ]*
+import Groq from 'groq-sdk'
+
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+let groq = new Groq({ apiKey: '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' })
+conn.aiSessions = conn.aiSessions ? conn.aiSessions : {}
+    
+if (!text) return conn.reply(m.chat, `❀ Ingresa un texto para hablar con la IA`, m)
+
+try {
+let { key } = await conn.sendMessage(m.chat, { text: '_❀ cargando respuesta_' }, { quoted: m })
+if (!(m.sender in conn.aiSessions))
+conn.aiSessions[m.sender] = [{ role: 'system', content: `Eres Meta AI una inteligencia artificial, responde de manera clara y concisa para que los usuarios entiendan mejor tus respuestas. El nombre del usuario será: ${conn.getName(m.sender)}`, }]
+  
+if (conn.aiSessions[m.sender].length > 10) {
+conn.aiSessions[m.sender] = conn.ia[m.sender].slice(-1)
+}
+
+conn.aiSessions[m.sender].push({ role: 'user', content: text, })
+
+let msg = [ ...conn.aiSessions[m.sender], { role: 'user', content: text, }]
+
+let payloads = { messages: msg, model: 'llama-3.1-70b-versatile' }
+
+let json = await groq.chat.completions.create(payloads)
+let msg = json.choices[0].message.content
+conn.aiSessions[m.sender].push({ role: "system", content: msg })
+    
+await conn.sendMessage(m.chat, { text: msg, edit: key }, { quoted: m })
+    
+} catch (error) {
+console.error(error)
+}}
+
+handler.command = ['metaai']
+
+export default handler

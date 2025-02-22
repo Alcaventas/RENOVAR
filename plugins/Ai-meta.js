129b3d1c8380c59e31613059f09656bf28858aab
Alba070503 <97574964+Alba070503@users.noreply.github.com>
2025-02-21 22:01:58 -0400

    Update Ai-meta.js

--- plugins/Ai-meta.js
+++ plugins/Ai-meta.js
@@ -9,7 +9,7 @@ const handler = async (m, { conn, text }) => {
 if (!text) return conn.reply(m.chat, '*Ingresa un texto para hablar con DeepSeek AI.*', m)
   
 try {
-let { data } = await axios.get(`https://archive-ui.tanakadomp.biz.id/ai/deepseek?text=${encodeURIComponent(text)}`)
+let { data } = await axios.get(`https://delirius-apiofc.vercel.app/ia/llamaia?query=${encodeURIComponent(text)}`)
 await m.reply(data?.result || '❌ No se obtuvo una respuesta válida de DeepSeek AI.')
 } catch {
 await m.reply('*❌ Error al procesar la solicitud.*')
